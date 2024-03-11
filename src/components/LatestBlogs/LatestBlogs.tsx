3f9d26d77e7677ae028a1b14ad2f28f1b2683b1a
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-03-11 12:08:52 +0600

    latest blog component

---
+++ src/components/LatestBlogs/LatestBlogs.tsx
@@ -0,0 +1,17 @@
+const LatestBlogs = () => {
+  return (
+    <div>
+      <h2 className="text-4xl text-center my-5">
+        Latest Blogs From <span className="text-accent">Blogiz</span>
+      </h2>
+      <p className="text-gray-400 text-xl text-center max-w-[60ch] mx-auto">
+        <i>
+          Dive into the fascinating world of quantum computing, where unlocking
+          unprecedented computational power.
+        </i>
+      </p>
+    </div>
+  );
+};
+
+export default LatestBlogs;
