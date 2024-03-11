1b41a8043da53972144531ed67a4f7a1b47d3621
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-03-11 18:04:31 +0600

    generateStaticParams

--- src/app/blogs/[blogId]/page.tsx
+++ src/app/blogs/[blogId]/page.tsx
@@ -1,9 +1,20 @@
 import BlogDetails from "@/components/ui/BlogDetails";
+import { TBlog } from "@/types";
 
 type TBlogDetailProps = {
   params: {
     blogId: string;
   };
+};
+
+// static site generation.
+export const generateStaticParams = async () => {
+  const res = await fetch("http://localhost:5000/blogs");
+  const blogs = await res.json();
+
+  return blogs.slice(0, 3).map((blog: TBlog) => ({
+    blogId: blog.id, // here blogId is name convention with file name.
+  }));
 };
 
 const BlogDetailPage = async ({ params }: TBlogDetailProps) => {
