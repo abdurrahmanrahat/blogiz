7dfa0d48ae20ec4731f43d6343779c2e4817104c
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-03-11 12:35:53 +0600

    blog page

---
+++ src/app/blogs/page.tsx
@@ -0,0 +1,31 @@
+import BlogCard from "@/components/ui/BlogCard";
+import { TBlog } from "@/types";
+
+const BlogsPage = async () => {
+  const res = await fetch("http://localhost:5000/blogs", {
+    cache: "no-store", // it does not save in build time. when user request, then data will come from database.
+  });
+  const blogs = await res.json();
+
+  return (
+    <div className="w-[90%] mx-auto my-16">
+      <h2 className="text-4xl text-center my-5">
+        All Blogs From <span className="text-accent">Blogiz</span>
+      </h2>
+      <p className="text-gray-400 text-xl text-center max-w-[60ch] mx-auto">
+        <i>
+          Dive into the fascinating world of quantum computing, where unlocking
+          unprecedented computational power.
+        </i>
+      </p>
+
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-8">
+        {blogs.map((blog: TBlog) => (
+          <BlogCard key={blog.id} blog={blog} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default BlogsPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 import LatestBlogs from "@/components/LatestBlogs/LatestBlogs";
 
 const HomePage = async () => {
-  const res = await fetch("http://localhost:5000/blogs");
+  const res = await fetch("http://localhost:5000/blogs", {
+    next: {
+      revalidate: 30,
+    }, // call the fetch method after every 30 seconds.
+  });
   const blogs = await res.json();
 
   return (
